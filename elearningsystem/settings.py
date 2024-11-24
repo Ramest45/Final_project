a63fa8f4db521aafa3d976216239da5ee9b7e44c
Ramest <167057087+Ramest45@users.noreply.github.com>
2024-11-24 11:16:36 +0530

    Update settings.py

--- elearningsystem/settings.py
+++ elearningsystem/settings.py
@@ -122,9 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'nouapp/static'),
-]
+
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR/'media'
 
