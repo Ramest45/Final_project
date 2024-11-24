5bf78083f28ddca268626de3758ebef6b44a8b1d
Ramest <167057087+Ramest45@users.noreply.github.com>
2024-11-24 10:25:14 +0530

    Update settings.py

--- elearningsystem/settings.py
+++ elearningsystem/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / 'nouapp/static',
+]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR/'media'
